db4fee4fb90e1fe0660535b9378cf94964e20973
April Weaver <april.weaver@revature.net>
2021-11-01 11:02:59 -0400

    added functionality but not tested

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Chef } from 'src/app/models/Chef';
+import { Recipe } from 'src/app/models/Recipe';
+import { RecipeDataService } from 'src/app/services/recipe-data.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  recipe!: Recipe;
 
-  ngOnInit(): void {
+  recipeTitle:string = this.recipe.title;
+  recipeBody:string = this.recipe.body;
+
+  editRecipe() {
+    this.recipe.title = this.recipeTitle;
+    this.recipe.body = this.recipeBody;
+    this.recipeService.editRecipe(this.recipe);
   }
+
+  constructor(private recipeService: RecipeDataService) { }
+
+  ngOnInit(): void { }
 
 }
