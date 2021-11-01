7af10e42868a7f373d19f18ce675c1865db0a356
April Weaver <april.weaver@revature.net>
2021-11-01 11:29:58 -0400

    Please add all

--- src/app/services/recipe-data.service.ts
+++ src/app/services/recipe-data.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../models/Recipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeDataService {
 
-  constructor() { }
+  private DATA_URL: string = "somethingsomething";
+
+  public editRecipe(recipe:Recipe) {
+    //update the Recipe in database
+  }
+
+  public deleteRecipe(recipe:Recipe) {
+    //delete the Recipe in database
+  }
+
+  constructor(private http: HttpClient) { }
 }
