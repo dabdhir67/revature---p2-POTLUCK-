426bcd3e272881e41d99a31383b6ce3b5c95c6f0
April Weaver <april.weaver@revature.net>
2021-11-02 14:18:54 -0400

    I dont remember at all what i added in here...

--- src/app/services/recipe-data.service.ts
+++ src/app/services/recipe-data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Recipe } from '../models/Recipe';
 
 @Injectable({
@@ -7,14 +8,14 @@ import { Recipe } from '../models/Recipe';
 })
 export class RecipeDataService {
 
-  private DATA_URL: string = "somethingsomething";
+  private DATA_URL: string = "http://localhost:8080/BackEnd/recipe";
 
   public editRecipe(recipe:Recipe) {
     //update the Recipe in database
   }
 
-  public deleteRecipe(recipe:Recipe) {
-    //delete the Recipe in database
+  public deleteRecipe(recipe:Recipe){
+    this.http.delete(this.DATA_URL, recipe);
   }
 
   constructor(private http: HttpClient) { }
