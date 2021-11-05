d6c786b971f67b84366d1d011579860938aa0c55
CJPete22 <cjpeterson7@wisc.edu>
2021-11-05 13:09:44 -0500

    restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ChefService } from './services/chef.service';
 import { LoginComponent } from './LoginView/login/login.component';
 import { KitchenComponent } from './KitchenView/kitchen/kitchen.component';
 import { MarketComponent } from './MarketView/market/market.component';
-import { AllRecipesComponent } from './KitchenView/all-recipes/all-recipes.component';
+import { AllRecipesComponent } from './MarketView/all-recipes/all-recipes.component';
 import { AllRecipesByChefComponent } from './KitchenView/all-recipes-by-chef/all-recipes-by-chef.component';
 
 
